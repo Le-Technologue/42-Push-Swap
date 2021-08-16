300694d75e97c011adb17e74f5aa0be88c3a25f4
William Cazorla <wetieven@student.42lyon.fr>
2021-08-16 22:33:29 +0200

    How to finish that rudimentary quick sort ? - On the spot edge sorting is getting better, but we should add a   condition to check if what we are sorting belongs to the same subset.   For now it tends to swap crazily whenever we bump into a high value   instead of putting it under the stack. - Delimitation of subsets for sorting and breaking conditions is still   an issue, maybe we should abandon our "store the pivot under the stack   to put it back on it" approach and resolve to pure calculations. - Also, should we empty our first stack or recursively sort our subsets   betweem both stacks ? - PS : Berenice is so cool she just made the git log

--- srcs/algo/psw_algo.c
+++ srcs/algo/psw_algo.c
@@ -6,71 +6,79 @@
 /*   By: wetieven <wetieven@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/07 20:39:53 by wetieven          #+#    #+#             */
-/*   Updated: 2021/08/14 22:33:22 by wetieven         ###   ########lyon.fr   */
+/*   Updated: 2021/08/16 19:42:43 by wetieven         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include "psw_inst.h"
 #include "psw_monitor.h"
 
-t_error	srt_edges(t_game *game)
+void	srt_tops(t_game *game)
 {
-	if (LOAD_A && STK_A[TOP_A].key > STK_A[0].key
+	if (LOAD_A > 1 && STK_A[TOP_A].key > STK_A[TOP_A - 1].key)
+		buf_inst(game, SA);
+	if (LOAD_B > 1 && STK_B[TOP_B].key < STK_B[TOP_B - 1].key)
+		buf_inst(game, SB);
+}
+
+void	srt_bottoms(t_game *game, char stack, size_t med)
+{
+	if (stack != 'B' && STK_A[0].key != med
+			&& LOAD_A > 3 && STK_A[TOP_A].key > STK_A[0].key
 			&& STK_A[TOP_A].key < STK_A[TOP_A - 1].key)
 		buf_inst(game, RRA);
-	if (LOAD_B && STK_B[TOP_B].key < STK_B[0].key
+	if (stack != 'A' && STK_B[0].key != med
+			&& LOAD_B > 3 && STK_B[TOP_B].key < STK_B[0].key
 			&& STK_B[TOP_B].key > STK_B[TOP_B - 1].key)
 		buf_inst(game, RRB);
+//	if (LOAD_A > 3 && STK_A[TOP_A].key > STK_A[0].key)
+//		buf_inst(game, RA);
+//	if (LOAD_B > 3 && STK_B[TOP_B].key < STK_B[0].key)
+//		buf_inst(game, RB);
+}
 
-	if (LOAD_A > 3 && STK_A[TOP_A].key > STK_A[0].key)
-		buf_inst(game, RA);
-	if (LOAD_B > 3 && STK_B[TOP_B].key < STK_B[0].key)
-		buf_inst(game, RB);
-
-	if (LOAD_A > 2 && STK_A[TOP_A].key > STK_A[TOP_A - 1].key)
-		buf_inst(game, SA);
-	if (LOAD_B > 2 && STK_B[TOP_B].key < STK_B[TOP_B - 1].key)
-		buf_inst(game, SB);
-
+void	srt_next_to_top(t_game *game)
+{
 	if (LOAD_A > 3 && STK_A[0].key == STK_A[TOP_A].key + 1
 			&& game->qty > 2)
 		buf_inst(game, RRA);
 	if (LOAD_B > 3 && STK_B[0].key == STK_B[TOP_B].key - 1
 			&& game->qty > 2)
 		buf_inst(game, RRB);
-	if (STK_A[TOP_A].key > STK_A[TOP_A - 1].key)
-		buf_inst(game, SA);
-	if (STK_B[TOP_B].key < STK_B[TOP_B - 1].key)
-		buf_inst(game, SB);
-
-	if (STK_A[TOP_A].key < STK_A[TOP_A - 1].key
-			&& STK_A[TOP_A - 1].key <= STK_A[0].key)
-		return (CLEAR);
-	return (ERROR);
+	srt_tops(game);
 }
 
-void	psw_qcksrt(t_game *game, size_t start, size_t end)
+void	srt_edges(t_game *game, char stack, size_t med)
+{
+	while ((LOAD_A && (STK_A[TOP_A].key > STK_A[TOP_A - 1].key))
+			|| (LOAD_B && (STK_B[TOP_B].key < STK_B[TOP_B - 1].key)))
+	{
+		srt_bottoms(game, stack, med);
+		srt_tops(game);
+//		srt_next_to_top(game);
+	}
+}
+// from last sorted value to last_pvt,
+// then from last_pvt to previous last_pvt
+void	psw_qcksrt_B(t_game *game, size_t end, size_t start)
 {
 	size_t	med;
 	size_t	to_sort;
-	_Bool	restore_med;
 
-	if (start >= end)
+	if (end - start <= 3)
 		return ;
-	restore_med = 0;
-	med = start + (end - start) / 2;
+	med = start + (end - last_pvt) / 2;
 	to_sort = (end - start) / 2 + 1;
-	while (to_sort) 
+	while (STK_B[end--].key != start)
 	{
+		srt_edges(game, 'B', med);
 		if (STK_A[TOP_A].key == med)
 		{
 			buf_inst(game, PB);
 			to_sort--;
 			if (to_sort > 0 && to_sort != (end - start) / 2)
-			{
 				buf_inst(game, RB);
-				restore_med = 1;
-			}
 		}
 		else if (STK_A[TOP_A].key < med)
 		{
@@ -80,13 +88,42 @@ void	psw_qcksrt(t_game *game, size_t start, size_t end)
 		else
 			buf_inst(game, RA);
 	}
-	if (restore_med || STK_A[0].key == med)
+	if (STK_B[0].key == med)
 		buf_inst(game, RRB);
-//	while (srt_edges(game))
-//		;
-//	psw_qcksrt(game, 0, med); //inverted logic here ?
-	psw_qcksrt(game, med + 1, end);
+	psw_qcksrt_B(game, 0, med);
+}
 
+void	psw_qcksrt_A(t_game *game, size_t start, size_t end)
+{
+	size_t	med;
+	size_t	to_sort;
+
+	if (end - start <= 3)
+		return ;
+	med = start + (end - start) / 2;
+	to_sort = (end - start) / 2 + 1;
+	while (to_sort) 
+	{
+		srt_edges(game, 'A', med);
+		if (STK_A[TOP_A].key == med)
+		{
+			buf_inst(game, PB);
+			to_sort--;
+			if (to_sort > 0 && to_sort != (end - start) / 2)
+				buf_inst(game, RB);
+		}
+		else if (STK_A[TOP_A].key < med)
+		{
+			buf_inst(game, PB);
+			to_sort--;
+		}
+		else
+			buf_inst(game, RA);
+	}
+	if (STK_B[0].key == med)
+		buf_inst(game, RRB);
+	vctr_push(game->pvt, med);
+	psw_qcksrt_A(game, med + 1, end);
 }
 
 void	psw_solver(t_game *game)
@@ -95,7 +132,7 @@ void	psw_solver(t_game *game)
 		psw_monitor(game, 0);
 //	while (srt_edges(game))
 //		;
-	psw_qcksrt(game, 0, TOP_A);
+	psw_qcksrt_A(game, 0, TOP_A);
 //	srt_edges(game);
 	buf_inst(game, END);
 }
