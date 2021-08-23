c85c9b717ade94ba671f80322f892937a70c4200
William Cazorla <wetieven@student.42lyon.fr>
2021-08-23 22:18:56 +0200

    Edited 5 val tester - Double check of the values correspondance between monitoring and   testing, fix of the 4 2 segment in consequence.

--- 5_val.sh
+++ 5_val.sh
@@ -139,6 +139,8 @@ echo "./push_swap 3 2 5 1 4"
 echo "./push_swap 3 2 5 4 1"
 ./push_swap 3 2 5 4 1
 
+# 3 4
+
 # 3 5
 
 # 4 1
@@ -157,17 +159,17 @@ echo "./push_swap 4 1 5 3 2"
 
 # 4 2
 echo "./push_swap 4 2 1 3 5"
-./push_swap 2 4 1 3 5
+./push_swap 4 2 1 3 5
 echo "./push_swap 4 2 1 5 3"
-./push_swap 2 4 1 5 3
+./push_swap 4 2 1 5 3
 echo "./push_swap 4 2 3 1 5"
-./push_swap 2 4 3 1 5
+./push_swap 4 2 3 1 5
 echo "./push_swap 4 2 3 5 1"
-./push_swap 2 4 3 5 1
+./push_swap 4 2 3 5 1
 echo "./push_swap 4 2 5 1 3"
-./push_swap 2 4 5 1 3
+./push_swap 4 2 5 1 3
 echo "./push_swap 4 2 5 3 1"
-./push_swap 2 4 5 3 1
+./push_swap 4 2 5 3 1
 
 # 4 3
 
