b92855c7fa1368a6939b28b965ada77cfb6cf516
William Cazorla <wetieven@student.42lyon.fr>
2021-09-16 11:37:02 +0200

    Fixed misspelled header protection

--- incs/psw_chk_dupl.h
+++ incs/psw_chk_dupl.h
@@ -6,12 +6,12 @@
 /*   By: wetieven <wetieven@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 10:51:27 by wetieven          #+#    #+#             */
-/*   Updated: 2021/09/16 11:13:33 by wetieven         ###   ########lyon.fr   */
+/*   Updated: 2021/09/16 11:36:48 by wetieven         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PSW_CHK_DUPL
-# define PSW_CHK_DUPL
+#ifndef PSW_CHK_DUPL_H
+# define PSW_CHK_DUPL_H
 
 t_error	psw_chk_dupl(t_game *game, long *buf);
 
